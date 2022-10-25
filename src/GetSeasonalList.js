f27df9eea8b61fb5b3c86928009ae471e6b1c27d
Ralph Alexander <ralph.halim103@gmail.com>
2022-10-24 21:41:46 -0700

    Update

--- src/GetSeasonalList.js
+++ src/GetSeasonalList.js
@@ -1,8 +1,9 @@
 
 import Axios from 'axios'
-import { AiOutlineSetting } from 'react-icons/ai'
+import { AiFillSetting, AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
+import { BiExit } from 'react-icons/bi'
 import { DateTime } from 'luxon'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css';
 import './ListView.css'
@@ -12,7 +13,8 @@ const USER_LIST_KEY = 'userList'
 const VIEW_KEY = 'view'
 const FILTER_TIME_KEY = 'filter_time'
 const SORTED_ASCENDING_TITLE_KEY = 'ascendingTitle'
-
+const SORTED_ASCENDING_DATE_KEY = 'ascendingTitle'
+const DISPLAY_TYPE_KEY = 'displayType'
 
 const timezone = DateTime.local().zoneName
 
@@ -21,43 +23,49 @@ export default function GetSeasonalList() {
 	const [seasonalList, setSeasonalList] = useState()
 	const [userList, setUserList] = useState([])
 	const [view, setView] = useState('seasonal view')
-	const [displayType, setDisplayType] = useState('MALStyle')
-	const [dropdownView, setDropdownView] = useState(false)
+	const [displayType, setDisplayType] = useState('RowDisplay')
+	const [settingsView, setSettingsView] = useState(false)
 	const [filterTime, setFilterTime] = useState(24)
-	const [titleIsSortedInAscending,setTitleIsSortedInAscending] = useState()
-	const [dateIsSortedInAscending, setDateIsSortedInAscending] = useState()
+	const [titleIsSortedInAscending,setTitleIsSortedInAscending] = useState('')
+	const [dateIsSortedInAscending, setDateIsSortedInAscending] = useState('')
 	
 
 	useEffect(()=>{
 		getData(1,true)
-
 		const localStorageUserList = JSON.parse(localStorage.getItem(USER_LIST_KEY))
-		if (localStorageUserList) {
-			setUserList(localStorageUserList)
-		}
+		if (localStorageUserList) setUserList(localStorageUserList)
 
 		const localStorageView = JSON.parse(localStorage.getItem(VIEW_KEY))
 		if (localStorageView) setView(localStorageView)
 
-		const localStorageTitleSort = JSON.parse(localStorage.getItem(SORTED_ASCENDING_TITLE_KEY))
-		if (localStorageTitleSort) setTitleIsSortedInAscending(localStorageTitleSort)
-
 		const localStorageFilterTime = JSON.parse(localStorage.getItem(FILTER_TIME_KEY))
 		if (localStorageFilterTime) setFilterTime(localStorageFilterTime)
+
+		const localStorageDisplayType = JSON.parse(localStorage.getItem(DISPLAY_TYPE_KEY))
+		if (localStorageDisplayType) setDisplayType(localStorageDisplayType)
+		
+		const localStorageTitleAscending = JSON.parse(localStorage.getItem(SORTED_ASCENDING_TITLE_KEY))
+		if (localStorageTitleAscending) setTitleIsSortedInAscending(localStorageTitleAscending)
+
+		const localStorageDateAscending = JSON.parse(localStorage.getItem(SORTED_ASCENDING_DATE_KEY))
+		if (localStorageDateAscending) setDateIsSortedInAscending(localStorageDateAscending)
 	},[])
 
 	useEffect(()=>{
-		localStorage.setItem(FILTER_TIME_KEY,JSON.stringify(filterTime))
+		localStorage.setItem(DISPLAY_TYPE_KEY, JSON.stringify(displayType))
+	},[displayType])
+
+	useEffect(()=>{
+		localStorage.setItem(FILTER_TIME_KEY, JSON.stringify(filterTime))
 	},[filterTime])
 
 	useEffect(()=>{
-		localStorage.setItem(VIEW_KEY,JSON.stringify(view))
-		
-		// localStorage.setItem(USER_LIST_KEY,JSON.stringify(userList))
+		localStorage.setItem(VIEW_KEY ,JSON.stringify(view))
 	},[view])
 
 	useEffect(()=>{
-		localStorage.setItem(USER_LIST_KEY,JSON.stringify(userList))
+		localStorage.setItem(USER_LIST_KEY, JSON.stringify(userList))
+		console.log(userList)
 		// console.log(localStorage.getItem(USER_LIST_KEY))
 		// setUserList(
 		// 	userList.sort((a,b) => {
@@ -68,9 +76,14 @@ export default function GetSeasonalList() {
 		// )
 	},[userList])
 
-	// useEffect(()=>{
-	// 	localStorage.seItem(SORTED_ASCENDING_TITLE_KEY, JSON.stringify(titleIsSortedInAscending))
-	// },[titleIsSortedInAscending])
+	useEffect(()=>{
+		localStorage.setItem(SORTED_ASCENDING_TITLE_KEY ,JSON.stringify(titleIsSortedInAscending))
+		console.log(titleIsSortedInAscending)
+	},[titleIsSortedInAscending])
+
+	useEffect(()=>{
+		localStorage.setItem(SORTED_ASCENDING_DATE_KEY, JSON.stringify(dateIsSortedInAscending))
+	},[dateIsSortedInAscending])
 
     const getData = async (pageNumber,hasPage) => {
 
@@ -90,7 +103,7 @@ export default function GetSeasonalList() {
 	}
 
 
-	function Seasonal({anime}) {
+	function SeasonalMAL({anime}) {
 
 		// handles adding an anime the to userList state
 		function handleClick(){
@@ -133,6 +146,45 @@ export default function GetSeasonalList() {
 		</>
 	  )
 	}
+	function SeasonalRow({anime}) {
+
+		// handles adding an anime the to userList state
+		function handleClick(){
+			setUserList((current)=>{
+				// if list is empty, add the anime to the list
+				if (userList === undefined) return [anime]
+				// if anime is not in user list, append it to the end of the list
+				else if (userList.find(element => element.mal_id === anime.mal_id) === undefined) return [...current, anime]
+				else return [...current]
+			})
+			
+		}
+	
+	  return (
+		<>
+			<div className='anime-card-row'>
+				<div className='anime-card-row-img-wrapper'>
+						<img 
+							src={anime.images.jpg.image_url} 
+							className='anime-card-row-img'/>
+							<div className='anime-card-row-background'/>
+					
+				</div>
+				<div className='anime-card-row-title'>
+					{anime.title}
+				</div>
+				<div className='anime-card-row-button-wrapper'>
+						<button 
+							className='anime-card-button'
+							onClick={handleClick} >
+								Add to List
+						</button>
+					</div>
+			</div>
+		</>
+	  )
+	}
+
 
 	function SeasonalView(){
 
@@ -143,34 +195,87 @@ export default function GetSeasonalList() {
 					Seasonal Anime
 				</h1>
 					<div className='card-container'>
-					{
-					// TODO
-					displayType === 'MALStyle' && 
+					{displayType === 'MALStyle' && 
 					seasonalList.map((seasonalAnime) =>{
 						return(
-								<Seasonal 
+								<SeasonalMAL 
 									anime={seasonalAnime}
 									key={seasonalAnime.mal_id}
 								/>
 								)
 							})}
+					{displayType === 'RowDisplay' && 
+					seasonalList.map((seasonalAnime) =>{
+						return(
+								<SeasonalRow 
+									anime={seasonalAnime}
+									key={seasonalAnime.mal_id}
+								/>
+								)
+							})}
+					{/* {displayType === 'CardDisplay' && 
+					seasonalList.map((seasonalAnime) =>{
+						return(
+								<SeasonalRow 
+									anime={seasonalAnime}
+									key={seasonalAnime.mal_id}
+								/>
+								)
+							})} */}
 					</div>
 					</div>
 			</>
 		)
 	}
 
+
 	function List({
 		anime
 	}){
-		
-		const [isSelected, setIsSelected] = useState(false)
-
-		const handleCheckBox = () => {
-
-			setIsSelected(!isSelected)
+		if (anime.episode===null || !anime.hasOwnProperty('episode')){
+			anime.episode = 0
 		}
-		anime.checked = isSelected
+		if (anime.checked===null || !anime.hasOwnProperty('checked')){
+			anime.checked = false
+		}
+		
+		const handleCheckBox = () => {
+			setUserList(
+				userList.map(current => {
+					if (current.mal_id === anime.mal_id){
+						return {...current, checked: !anime.checked}
+					}
+					return current
+				})
+			)
+		}
+			
+		const handleIncreaseEpisodeCounter = () => {
+			setUserList(
+				userList.map(current => {
+					if (current.mal_id === anime.mal_id){
+						if (anime.episode && anime.episode === anime.episodes){
+							return current
+						}
+						return {...current, episode: anime.episode+1}
+					}
+					return current
+				})
+			)
+		}
+		const handleDecreaseEpisodeCounter = () => {
+			setUserList(
+				userList.map(current => {
+					if (current.mal_id === anime.mal_id){
+						if (anime.episode===0){
+							return current
+						}
+						return {...current, episode: anime.episode-1}
+					}
+					return current
+				})
+			)
+		}
 
 		var startDate, startMonth
 		if (anime.aired.prop.from.day < 10){
@@ -180,35 +285,65 @@ export default function GetSeasonalList() {
 			startMonth = '0' + anime.aired.prop.from.month
 		} else startMonth = anime.aired.prop.from.month
 
-		var formattedDate = String(
-			(anime.aired.prop.from.year) + '-' + startMonth + '-' + 
-			startDate + ' ' + anime.broadcast.time
-		) 
+		
+		var formattedDate 
+		if (anime.broadcast.time){
+			formattedDate = String((anime.aired.prop.from.year) + '-' +
+			startMonth + '-' + startDate + ' ' + anime.broadcast.time
+		)} else {
+			formattedDate = (anime.aired.prop.from.year) + '-' +
+			startMonth + '-' + startDate
+			console.log(formattedDate)
+		}
 
 		
 		var date = DateTime.fromSQL(formattedDate, { zone: "Asia/Tokyo" })
 		date = date.setZone(timezone)
-
+		
+		// if (anime.hasOwnProperty(''))
 
 		return(
 			<>
-				<tr className="cell">
+				<tr>
 					<td>
-						<input 
-							type='checkbox'
-							checked={isSelected}
-							onChange={handleCheckBox}
-							
-						/>
+						<label>
+							<input 
+								type='checkbox'
+								checked={anime.checked}
+								onChange={handleCheckBox}
+								/>
+						</label>
 					</td>
 					<td>
-						<img 
-						src={anime.images.jpg.image_url}
-						className='list-view-img'/>
+						<div className='list-view-img-wrapper'>
+							<img 
+							src={anime.images.jpg.image_url}
+							className='list-view-img'/>
+						</div>
 					</td>
 					<td>{anime.title}</td>
-					<td></td>
-					<td>{date.weekdayLong}, {date.hour < 10 && '0'}{date.hour}:{date.minute === 0 ? '00' : date.minute}</td>
+					<td>						
+						<div className='progress-flex'>
+							<div className='minus-icon-wrapper'>
+								<AiOutlineMinus onClick={handleDecreaseEpisodeCounter}
+								className='plus-minus-icon' /> 
+							</div>
+							<div className='counter'>
+								{anime.episode} / {anime.episodes ? anime.episodes : '?'} 
+							</div>
+							<div className='plus-icon-wrapper'>
+								<AiOutlinePlus onClick={handleIncreaseEpisodeCounter}
+								className='plus-minus-icon' /> 
+							</div>
+						</div>
+						
+					</td>
+					<td className='table-time'>
+						<div>
+							{date.weekdayLong}, {date.hour < 10 && '0'}{date.hour}:
+							{date.minute === 0 ? '00' : date.minute} 
+						</div>{!anime.broadcast.time && '(Assumed to be 00:00 JST)'}
+					</td>
 				</tr>
 			</>
 		)
@@ -225,12 +360,18 @@ export default function GetSeasonalList() {
 				return ((a.title > b.title) ? 1 : -1)
 			})
 		}
-		setUserList(sortedUserList)
+		setUserList(
+			sortedUserList.map(current=>{
+				return current
+			}))
+
 		if (titleIsSortedInAscending || !titleIsSortedInAscending) setTitleIsSortedInAscending(!titleIsSortedInAscending)
 		else setTitleIsSortedInAscending(true)
-
-		setDateIsSortedInAscending(null)
+		setDateIsSortedInAscending('')
 	}
+
+	// TODO
+	// update the logic a bit: instead of todays date, use an arbitrary date (monday or sunday)
 	function handleDateSort() {
 		var sortedUserList
 		if (dateIsSortedInAscending){
@@ -242,50 +383,59 @@ export default function GetSeasonalList() {
 				return ((getTime(a) > getTime(b)) ? 1 : -1)
 			})
 		}
-		setUserList(sortedUserList)
+		setUserList(
+			sortedUserList.map(current=>{
+				return current
+			}))
 		if (dateIsSortedInAscending || !dateIsSortedInAscending) setDateIsSortedInAscending(!dateIsSortedInAscending)
 		else setDateIsSortedInAscending(true)
-		
-		setTitleIsSortedInAscending(null)
+		setTitleIsSortedInAscending('')
 	}
 
 	const MyListView = () =>{
 
 
-
+// TODO
+// possible idea: add a "are you sure button when removing"
 		return (
 			<>
 				<h1>
 					My Seasonal List
 				</h1>
+				<div className='button-container'>
+					<button onClick={handleRemoveSelected}> Remove Selected </button>
+					<button onClick={handleRemoveAll}> Remove All </button>
+				</div>
+				<div className='table-container'>
 				
-				<table>
-					<thead> 
-						<tr>
-							<th></th>
-							<th> Image </th>
-							<th onClick={handleTitleSort}> 
-								Name
-							 {/* have an arrow pointing either up or down depending on 
-							 titleIsSortedInAscending */}
-							 </th>
-							<th> Your Progress </th>
-							<th onClick={handleDateSort}>
-								Air Date and Air Time  
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-					{userList.length > 0 && userList.map((seasonalAnime) =>{
-						
-						return(
-							<List 
-								anime={seasonalAnime}
-								key={seasonalAnime.mal_id}
-							/>
-								)})}
-					</tbody>
-				</table>
+					<table>
+						<thead> 
+							<tr>
+								<th></th>
+								<th> Image </th>
+								<th onClick={handleTitleSort}> 
+									Name
+								{/* have an arrow pointing either up or down depending on 
+								titleIsSortedInAscending */}
+								</th>
+								<th> Your Progress </th>
+								<th onClick={handleDateSort}>
+									Air Date and Air Time  
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+						{userList.length > 0 && userList.map((seasonalAnime) =>{
+							return(
+								<List 
+									anime={seasonalAnime}
+									key={seasonalAnime.mal_id}
+								/>
+									)})}
+						</tbody>
+					</table>
+				
+				</div>
 			</>
 		)
 
@@ -349,6 +499,8 @@ export default function GetSeasonalList() {
 	}
 
 	function getTime(anime) {
+		if (!anime.broadcast.time) return 50
+		
 		var startDate, startMonth
 		if (anime.aired.prop.from.day < 10){
 			startDate = '0' + anime.aired.prop.from.day
@@ -366,11 +518,13 @@ export default function GetSeasonalList() {
 		var date = DateTime.fromSQL(formattedDate, { zone: "Asia/Tokyo" })
 		date = date.setZone(timezone)
 
+		// change to midnight monday or sunday
 		var currentTime = DateTime.now().setZone(timezone)
 		var diffInWeeks = currentTime.diff(date,'weeks')
 		
-		date = date.plus({weeks:Math.abs(Math.ceil(diffInWeeks.values.weeks))})
+		// TODO broken :(
 		
+		date = date.plus({weeks:Math.abs(Math.ceil(diffInWeeks.values.weeks))})
 		return date.diff(currentTime,['hours'])
 		}
 
@@ -440,7 +594,6 @@ export default function GetSeasonalList() {
 		)
 	}
 
-
 	function handleSeasonalAnimeViewClick(){
 		setView('seasonal view')
 	}
@@ -458,59 +611,72 @@ export default function GetSeasonalList() {
 		setUserList(userList.filter(anime => !anime.checked))
 	}
 
+	function handleChangeSettings(e){
+		setDisplayType(e.target.value)
+	}
+
 	//TODO
 	//Fix the select stuff
-	function DropdownComponent() {
+	function SettingsComponent() {
 		return (
 			
 				<div className='settings-window-wrapper'>
-					<div className='exit-button'onClick={dropdown}> X </div>
+					<div className='exit-button'onClick={handleClickSettings}>
+						<BiExit className='exit-icon'/>
+					</div>
 					<div className='settings-window'>
-						Settings:
-						<select
-							value={displayType}
-							onChange={(e)=> {setDisplayType(e.target.value)}}
-						>
-							<option value='MALStyle'>MAL Style</option>
-							<option value='RowDisplay'>Row Display</option>
-							<option value='CardDisplay'>Card Display</option>
-						</select>
+						<div className='settings-header'>
+							Select Layout:
+						</div>
+						<div className='options'>
+							<input type="radio" value="RowDisplay" name="style" 
+							onChange={handleChangeSettings}
+							className='radio'
+							checked={displayType === 'RowDisplay'}
+							/> 
 
+							<input type="radio" value="MALStyle" name="style" 
+							onChange={handleChangeSettings}
+							className='radio'
+							checked={displayType === 'MALStyle'}
+							/> 
+						</div>
+					<div className='layout1'>
+						<img src={process.env.PUBLIC_URL + '/RowDisplay.png'} />
+					</div>
+					<div className='layout2'>
+						<img src={process.env.PUBLIC_URL + '/MALDisplay.png'} />
+					</div>
 					</div>
 				</div>
 		
 		)
 	}
 
-	function dropdown() {
-		setDropdownView(!dropdownView)
+	function handleClickSettings() {
+		setSettingsView(!settingsView)
 	}
 
 	if (seasonalList!==undefined){		
 	return (
 		<>
-		{dropdownView && <DropdownComponent />}
-			<div 
-			className={dropdownView ? 'z-index':''}
-			>
-				<div className='seasonal-list-view-container'>
-					<div className='seasonal-list-view-mal' onClick={handleSeasonalAnimeViewClick}> Seasonal Anime Viewer </div>
-					<div className='seasonal-list-view-rows' onClick={handleSeasonalAnimeViewClick}> </div>
-				</div>
-				<div>
-					{/* <div className='settings-wrapper'> */}
-						<AiOutlineSetting 
-						onClick={dropdown}
+		{settingsView && <SettingsComponent />}
+			<div className={settingsView ? 'z-index':''}>
+				<nav className='nav-header'>
+					<div className='settings-icon-wrapper'>
+					<AiFillSetting 
+						onClick={handleClickSettings}
 						className='settings-icon'/>
-					{/* </div> */}
-					<div className='center'>
-						<div className='center' onClick={handleMyListViewClick}> My List </div>
-						<div className='center' onClick={handleSeasonalCalendarClick}> My Seasonal Calendar </div>
 					</div>
-				</div>
+					<div className={view === 'seasonal view' ? 'nav-elements-focused' : 'nav-elements'} onClick={handleSeasonalAnimeViewClick}> Seasonal Anime </div>
+					<div className='separator'> | </div>  
+					<div className={view === 'list view' ? 'nav-elements-focused' : 'nav-elements'} onClick={handleMyListViewClick}> My Seasonal List </div> 
+					<div className='separator'> | </div>  
+					<div className={view === 'seasonal calendar view' ? 'nav-elements-focused' : 'nav-elements'} onClick={handleSeasonalCalendarClick}> My Seasonal Calendar </div> 
+				</nav>						
 				<div>
-					<div className='center' onClick={handleRemoveAll}> Remove All </div>
-					<div className='center' onClick={handleRemoveSelected}> Remove Selected </div>
+					{/* <div className='center' onClick={handleRemoveAll}> Remove All </div>
+					<div className='center' onClick={handleRemoveSelected}> Remove Selected </div> */}
 				</div>
 				{view === 'seasonal view' && <SeasonalView />}
 				{view === 'list view' && <MyListView />}
@@ -519,7 +685,5 @@ export default function GetSeasonalList() {
 			
 		</>
   )
-}
-			
-	
+}	
 }
